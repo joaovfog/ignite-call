24a756a710e325bfca9b2d65fe66ed4cda22f2f6
joaovfog <j.vitor48@outlook.com>
2023-12-18 19:29:59 -0300

    feat: eslint config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,5 +5,5 @@ import { globalStyles } from '@/styles/global'
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+  return <Component {...pageProps} />
 }

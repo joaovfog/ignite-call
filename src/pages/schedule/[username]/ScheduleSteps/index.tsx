7d0a482b55eb8899f00d0e2b5d4fc2aab3780ea8
joaovfog <j.vitor48@outlook.com>
2024-01-15 18:10:12 -0300

    wip: confirm step page

--- src/pages/schedule/[username]/ScheduleSteps/index.tsx
+++ src/pages/schedule/[username]/ScheduleSteps/index.tsx
@@ -1,5 +1,6 @@
-import { CalendarStep } from './CalendarStep'
+// import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleSteps() {
-  return <CalendarStep />
+  return <ConfirmStep />
 }
